ca634923aac6f3f17e62e7daef8a85c2bca0f520
FabioNilo <fabionilosoares@gmail.com>
2024-11-07 10:22:14 -0300

    atualizações rotas

--- src/app/form/form-login/form-login.component.ts
+++ src/app/form/form-login/form-login.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-form-login',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule,RouterLink],
   templateUrl: './form-login.component.html',
   styleUrl: './form-login.component.css'
 })
